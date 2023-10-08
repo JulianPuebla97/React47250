15c34343ddd6ffbe1f7878e4646c26e76acbb54a
julianperry97 <julianemmanuelpuebla@gmail.com>
2023-10-07 23:50:09 -0300

    pre entrega 1

--- src/componentes/ProductoBoton/ProductoBoton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './ProductoBoton.css';
-const ProductoBoton = () => {
-    return (
-        <button className='product-button'>Agregar al carrito</button>
-    )
-}
-
-export default ProductoBoton
